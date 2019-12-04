33c0e846d4bec16e10ed4abcac95599256518ef7
link-o-v <56926349+link-o-v@users.noreply.github.com>
2019-12-04 10:57:43 +0200

    popups

---
+++ script.js
@@ -0,0 +1,18 @@
+// Get the modal
+var popup = document.getElementById('avkPopup');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
+// Get the modal
+var popupCourses = document.getElementById('coursesPopup');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
